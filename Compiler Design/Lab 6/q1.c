190054df8a8adae512f939ba5f2d34758565d23e
Vaibhav-Sandhir <vaibhavsandhir1@gmail.com>
2023-09-18 02:22:29 +0000

    Lab 6 done

---
+++ Compiler Design/Lab 6/q1.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int curr = 0;
+char str[256];
+
+void S();
+void T();
+void Tprime();
+
+void invalid(){
+	printf("\n--ERROR--\n");
+	exit(-1);
+}
+
+void valid(){
+	printf("\n--SUCCESS--\n");
+	exit(0);
+}
+
+void S(){
+	if(str[curr] == 'a'){
+		curr++;
+		valid();
+	}
+	else if(str[curr] == '>'){
+		curr++;
+		valid();
+	}
+	else if(str[curr] == '('){
+		printf("hello");
+		curr++;
+		T();
+		if(str[curr] == ')'){
+			curr++;
+			valid();
+		}
+		else{
+			invalid();
+		}
+	}
+	else{
+		invalid();
+	}
+}
+
+void T(){
+	if(str[curr] == 'a'){
+		curr++;
+		Tprime();
+	}
+	else if(str[curr] == '>'){
+		curr++;
+		Tprime();
+	}
+	else if(str[curr] == '('){
+		curr++;
+		T();
+		if(str[curr] == ')'){
+			curr++;
+			Tprime();
+		}
+		else{
+			invalid();
+		}
+	}
+	else{
+		invalid();
+	}
+}
+
+void Tprime(){
+	if(str[curr] == ','){
+		curr++;
+		S();
+		Tprime();
+	}
+}
+
+
+
+void main(){
+	printf("\nEnter input String: ");
+	scanf("%s", str);
+	S();
+	if(str[curr] == '$')
+		valid();
+	else
+		invalid();	
+}
---
+++ Compiler Design/Lab 6/q3.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int curr = 0;
+char str[100];
+
+void S();
+void A();
+void Aprime();
+void B();
+
+void valid(){
+	printf("\n--SUCCESS--\n");
+	exit(0);
+}
+
+void invalid(){
+	printf("\n--ERROR--\n");
+	exit(-1);
+}
+
+void S(){
+	if(str[curr] == 'a'){
+		curr++;
+		A();
+		if(str[curr] == 'c'){
+			curr++;
+			B();
+			if(str[curr] == 'e'){
+				curr++;
+				valid();
+			}
+			else{
+				invalid();
+			}
+		}
+		else{
+			invalid();
+		}
+	}
+	else{
+		invalid();
+	}
+}
+
+void A(){
+	if(str[curr] == 'b'){
+		curr++;
+		Aprime();
+	}
+	else{
+		invalid();
+	}
+}
+
+void Aprime(){
+	if(str[curr] == 'b'){
+		curr++;
+		Aprime();
+	}
+}
+
+void B(){
+	if(str[curr] == 'd'){
+		curr++;
+		valid();
+	}
+}
+
+void main(){
+	printf("\nEnter String: ");
+	scanf("%s", str);
+	S();
+	if(str[curr] == '$')
+		valid();
+	else
+		invalid();	
+}
